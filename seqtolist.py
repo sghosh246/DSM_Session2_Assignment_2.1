567ea862d395908f47516210ed9b3a948ce56a2a
sghosh246 <sghosh246@gmail.com>
2018-04-17 14:07:41 +0530

    Add files via upload

---
+++ seqtolist.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 11:50:46 2018
+@author: Sourav Ghosh
+"""
+numstr=input("Enter comma separated numbers: ")
+numlist=numstr.split(",")
+print(numlist)
